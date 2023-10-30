0b7cab09af9b6ca8e1768ca264dabf77ace0fc41
Martinez <amramart@hyster-yale.com>
2023-10-30 10:51:02 -0600

    Segundo Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MiComponente = () => {
+  return (
+    <div>
+      <h1>Hola desde un componente funcional con función flecha</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <MiComponente />
+    </>
+  );
+}
+
+export default App;
