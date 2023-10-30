db8526329d56f43859895803fdb104b04a1ad886
Martinez <amramart@hyster-yale.com>
2023-10-30 11:18:29 -0600

    Segundo Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import './Mimagen.css'; 
 
-const MiComponente = () => {
+const Mimagen = (props) => {
   return (
-    <div>
-      <h1>Hola desde un componente funcional con función flecha</h1>
-    </div>
+    <img alt={props.alt} src={props.path} className="imagenPersonalizada"></img>
   );
 };
 
 function App() {
   return (
     <>
-      <MiComponente />
+      <Mimagen alt="hey" path="https://images.unsplash.com/photo-1599597098541-4a851d567ce5?auto=format&fit=crop&q=80&w=1917&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+      <h1>Tu los Lunes 😭</h1>
     </>
   );
 }
