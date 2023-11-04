65917467263f43e3fcfbe17b9b92a1832dc302ac
Jarasdeboy <raul.jaramillo@outlook.com>
2023-11-03 21:03:17 -0600

    jelo

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Mimagen alt="hey" path="https://images.unsplash.com/photo-1599597098541-4a851d567ce5?auto=format&fit=crop&q=80&w=1917&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-      <h1>Tu los Lunes 😭</h1>
+      <h1>Jaime y su pc gei😭</h1>
     </>
   );
 }
