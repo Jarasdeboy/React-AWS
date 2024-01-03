3754a5d6f68288240cf07754f096340b83f52960
Jarasdeboy <raul.jaramillo@outlook.com>
2024-01-02 19:56:19 -0600

    last hit

--- src/components/Foodcard.jsx
+++ src/components/Foodcard.jsx
@@ -1,16 +1,20 @@
-const Foodcard = () => {
-    return (
-      <div className="w-full md:w-11/12 h-24 shadow-md  bg-white rounded-2xl m-4 flex justify-center items-center p-2">
-        <div className="w-full h-full rounded-2xl grid grid-cols-2">
-          <img src="https://img.freepik.com/foto-gratis/surtido-arepas-relleno-plato_23-2148742392.jpg?size=626&ext=jpg&ga=GA1.1.58449625.1699456218&semt=sph" alt="Descripción de la imagen" className="object-cover w-auto h-20 rounded-2xl" />
-          <div className="flex flex-col">
-            <h1 className="text-lg font-semibold text-black font-sans max-w-sm ...">Gorditas Becerra</h1>
-            <label className="text-sm font-normal text-gray-400 font-sans">$ Mexicana/gorditas</label>
-            <label className="text-base font-light font-mono text-black">MX$26</label>
-          </div>
+import React from 'react';
+
+const Foodcard = (props) => {
+  const { imageUrl } = props; // Obtener la URL de la imagen de las props
+
+  return (
+    <div className="w-full md:w-11/12 h-24 shadow-md bg-white rounded-2xl m-4 flex justify-center items-center p-2">
+      <div className="w-full h-full rounded-2xl grid grid-cols-2">
+        <img src={imageUrl} alt="Descripción de la imagen" className="object-cover w-auto h-20 rounded-2xl" />
+        <div className="flex flex-col">
+          <h1 className="text-lg font-semibold text-black font-sans max-w-sm ...">Gorditas Becerra</h1>
+          <label className="text-sm font-normal text-gray-400 font-sans">$ Mexicana/gorditas</label>
+          <label className="text-base font-light font-mono text-black">MX$26</label>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export { Foodcard };
+export { Foodcard };
--- src/components/Gridfood.jsx
+++ src/components/Gridfood.jsx
@@ -1,10 +1,18 @@
 import { Foodcard } from "./Foodcard";
 const Gridfood = () => {
-    return (
-      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 bg-yellow-400 h-screen place-content-start">
-        <Foodcard />
-      </div>
-    );
-  }
 
-  export { Gridfood };
+    const imageUrls = [
+        'https://img.freepik.com/foto-gratis/surtido-arepas-relleno-plato_23-2148742392.jpg?size=626&ext=jpg&ga=GA1.1.58449625.1699456218&semt=sph',
+        'https://img.freepik.com/foto-gratis/vista-superior-olla-relleno-arepas-salsa_23-2148742408.jpg?size=626&amp;ext=jpg&amp;ga=GA1.1.58449625.1699456218&amp;semt=sph',
+        'https://img.freepik.com/fotos-premium/barbacoa-tradicional-desmenuzada-cerdo-paleta-desmenuzada-cerdo-cocinada-fuego-lento-jugosa-carne-cerdo-cocinada-ahumador-lento_147620-3337.jpg',
+    ];
+    return (
+        <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 bg-yellow-400 h-screen place-content-start">
+            {imageUrls.map((imageUrl, index) => (
+                <Foodcard key={index} imageUrl={imageUrl} />
+            ))}
+        </div>
+    );
+}
+
+export { Gridfood };
